7635fa1ec9223ae9b1067ab9bf7c687c225866aa
DevinShelly <devin.shelly@gmail.com>
2017-06-23 02:36:01 -0400

    Update jobs.js
    Tracks the amount of hours worked each day and displays it next to the current speed on an open job

--- jobs.js
+++ jobs.js
@@ -1,63 +1,199 @@
+/*!
+ * JavaScript Cookie v2.1.4
+ * https://github.com/js-cookie/js-cookie
+ *
+ * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
+ * Released under the MIT license
+ */
+;(function (factory) {
+	var registeredInModuleLoader = false;
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+		registeredInModuleLoader = true;
+	}
+	if (typeof exports === 'object') {
+		module.exports = factory();
+		registeredInModuleLoader = true;
+	}
+	if (!registeredInModuleLoader) {
+		var OldCookies = window.Cookies;
+		var api = window.Cookies = factory();
+		api.noConflict = function () {
+			window.Cookies = OldCookies;
+			return api;
+		};
+	}
+}(function () {
+	function extend () {
+		var i = 0;
+		var result = {};
+		for (; i < arguments.length; i++) {
+			var attributes = arguments[ i ];
+			for (var key in attributes) {
+				result[key] = attributes[key];
+			}
+		}
+		return result;
+	}
+
+	function init (converter) {
+		function api (key, value, attributes) {
+			var result;
+			if (typeof document === 'undefined') {
+				return;
+			}
+
+			// Write
+
+			if (arguments.length > 1) {
+				attributes = extend({
+					path: '/'
+				}, api.defaults, attributes);
+
+				if (typeof attributes.expires === 'number') {
+					var expires = new Date();
+					expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+					attributes.expires = expires;
+				}
+
+				// We're using "expires" because "max-age" is not supported by IE
+				attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
+
+				try {
+					result = JSON.stringify(value);
+					if (/^[\{\[]/.test(result)) {
+						value = result;
+					}
+				} catch (e) {}
+
+				if (!converter.write) {
+					value = encodeURIComponent(String(value))
+						.replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+				} else {
+					value = converter.write(value, key);
+				}
+
+				key = encodeURIComponent(String(key));
+				key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+				key = key.replace(/[\(\)]/g, escape);
+
+				var stringifiedAttributes = '';
+
+				for (var attributeName in attributes) {
+					if (!attributes[attributeName]) {
+						continue;
+					}
+					stringifiedAttributes += '; ' + attributeName;
+					if (attributes[attributeName] === true) {
+						continue;
+					}
+					stringifiedAttributes += '=' + attributes[attributeName];
+				}
+				return (document.cookie = key + '=' + value + stringifiedAttributes);
+			}
+
+			// Read
+
+			if (!key) {
+				result = {};
+			}
+
+			// To prevent the for loop in the first place assign an empty array
+			// in case there are no cookies at all. Also prevents odd result when
+			// calling "get()"
+			var cookies = document.cookie ? document.cookie.split('; ') : [];
+			var rdecode = /(%[0-9A-Z]{2})+/g;
+			var i = 0;
+
+			for (; i < cookies.length; i++) {
+				var parts = cookies[i].split('=');
+				var cookie = parts.slice(1).join('=');
+
+				if (cookie.charAt(0) === '"') {
+					cookie = cookie.slice(1, -1);
+				}
+
+				try {
+					var name = parts[0].replace(rdecode, decodeURIComponent);
+					cookie = converter.read ?
+						converter.read(cookie, name) : converter(cookie, name) ||
+						cookie.replace(rdecode, decodeURIComponent);
+
+					if (this.json) {
+						try {
+							cookie = JSON.parse(cookie);
+						} catch (e) {}
+					}
+
+					if (key === name) {
+						result = cookie;
+						break;
+					}
+
+					if (!key) {
+						result[name] = cookie;
+					}
+				} catch (e) {}
+			}
+
+			return result;
+		}
+
+		api.set = api;
+		api.get = function (key) {
+			return api.call(api, key);
+		};
+		api.getJSON = function () {
+			return api.apply({
+				json: true
+			}, [].slice.call(arguments));
+		};
+		api.defaults = {};
+
+		api.remove = function (key, attributes) {
+			api(key, '', extend(attributes, {
+				expires: -1
+			}));
+		};
+
+		api.withConverter = init;
+
+		return api;
+	}
+
+	return init(function () {});
+}));
+
 //Adds the following functunality to within a job
 // Ctr + [ decreases playback speed by 0.1
 // Ctr + ] increases playback speed by 0.1
 // Double tapping Shift + Space will toggle between a speed of 1.0 and your previous non-1.0 speed
 // Also increases the maximum speed from 2.0 to 4.0, at which point the audio cuts out
 
-//Database functions
-db = null;
-
-createDatabase = function()
+offsetDate = function()
 {
-    db.transaction(function(tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS Time (id PRIMARY KEY AUTOINCREMENT, start, end)', [], updateDatabase);
-    });
+    //This starts a new day at 6 AM
+    offset = 6 * 1000 * 3600;
+    return new Date(Date.now() - offset);
 }
-                   
-insertOrUpdateTime = function(tx, results)
-{
-    len = results.rows.length;
-    if (len === 0)
-    {
-        tx.transaction(function(tx) {
-            tx.executeSql('INSERT INTO Time (start, end) VALUES (?, ?)', [Date.getTime(), Date.getTime()]);
-        });
-    }
-    else
-    {
-        id = results.rows.item[0]['id'];
-        tx.transaction(function(tx) {
-            tx.executeSql('UPDATE Time (end) VALUES (?) WHERE id = (?)', [Date.getTime(), id]);
-        });
-    }
-}
-    
-updateDatabase = function()
-{
-    if (!db)
-    {
-        db = openDatabase('3playDB', '1.0', 'Tracks time spent editing', 1024*1024, createDatabase);
-        return;
-    }
-    
-    threshhold = 60;
-    db.transaction(function(tx) {
-        tx.executeSql("SELECT id FROM Time WHERE (?) - end < (?)", [Date.getTime(), threshhold * 1000], insertOrUpdateTime);
-    });
-}        
 
 //Speed functions
 updateDisplay = function(display)
 {
+    working_time = parseInt(Cookies.get('working_time'));
+    hours = Math.floor(working_time/1000/3600);
+    minutes = Math.floor((working_time-hours * 1000 * 3600)/60/1000);
+    seconds = Math.floor((working_time-hours*1000*3600 - minutes*1000*60)/1000);
     while ($("#speed-display").length === 0)
     {
         $($(".btn-group:last")).after("<div class = 'btn-group' id = 'speed-display'></div>")
     }
-    $("#speed-display").text(display);
+    $("#speed-display").text("Speed: " + display + " | Time clocked: " + hours + "h, " + minutes + "m");//, " + seconds + "s");
 }
 
 changeSpeed = function(changeBy)
 {
+//     <input type="range" ng-model="ctrl.userSetting.video_playback_rate" min="0.5" max="2" step="0.1" class="ng-valid ng-not-empty ng-dirty ng-valid-parse ng-touched">
     speed = $("*[ng-model='ctrl.userSetting.video_playback_rate']");
     speed.attr("max", 4);
     speed.val(parseFloat(speed.val()) + changeBy);
@@ -88,7 +224,26 @@ document.onkeydown = function(e)
     threshhold = 1.0;
     if (Math.random() < threshhold)
     {
-        //updateDatabase();
+        now = offsetDate();
+        if (Cookies.get('current_date') != now.toDateString())
+        {
+            Cookies.set('current_date', now.toDateString());
+            Cookies.set('working_time', 0);
+        }
+        last_keypress = parseInt(Cookies.get('last_keypress'));
+        if(isNaN(last_keypress))
+        {
+            last_keypress = now.getTime();
+        }
+        working_time = parseInt(Cookies.get('working_time'));
+        Cookies.set('last_keypress', now.getTime());
+        elapsed_time = Math.max(0, now.getTime() - last_keypress);
+        if (elapsed_time < 5000)
+        {
+            working_time = working_time + elapsed_time;
+            Cookies.set('working_time', working_time);
+        }
+        changeSpeed(0.0);
     }
     
     leftBracket = 219;
