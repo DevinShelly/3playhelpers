7e4fc4d1eb78e801a8f77fcf2b0484d4964b2033
DevinShelly <devin.shelly@gmail.com>
2021-02-07 15:04:45 -0500

    Reduced the number of time updates to once per second

--- jobs.js
+++ jobs.js
@@ -172,14 +172,20 @@ setSpeed = function(newSpeed, updatePrev = true)
   changeSpeed(newSpeed - currentSpeed, updatePrev);
 }
 
+last_keypress = new Date().getTime();
 updateTimeWorked = function() 
 {
-  if(!scope())
+  now = new Date();
+  if(!scope() || now.getTime() - last_keypress < 1000)
   {
     return;
   }
   
-  now = new Date();
+  console.log(last_keypress);
+  console.log(now.getTime());
+  console.log(now.getTime() - last_keypress);
+  
+  
   midnight = new Date(now);
   midnight.setHours(24, 0, midnight_offset*60*60, 0);
 
